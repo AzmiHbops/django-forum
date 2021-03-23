5ef5f732c9a569556286ea999ed8f035c8a57db2
Victor <ademijuvioctor@gmail.com>
2021-03-22 18:15:52 -0700

    Ad views update

--- advert/views.py
+++ advert/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 # Create your views here.
 
 def advert(request):
-    pass
+    if request.method == "POST":
+        return redirect('post:home')
+    return render(request, "advert/ad_content.html")
